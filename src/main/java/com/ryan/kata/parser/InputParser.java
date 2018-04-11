0a6653fa502eff9c6abe95ec3c3e5b32f5ef3322
Ryan T <ryan.turinsky@gmail.com>
2018-04-10 21:48:24 -0400

    Testing the contents of the header parser

--- src/main/java/com/ryan/kata/parser/InputParser.java
+++ src/main/java/com/ryan/kata/parser/InputParser.java
@@ -1,15 +1,20 @@
 package com.ryan.kata.parser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputParser {
 
-
-    public List<String> parseHeader(String filePath) {
+    public List<String> parseHeader(String filePath) throws IOException {
         List<String> headerWords = new ArrayList<>();
 
-        headerWords.add(" ");
+        String header = Files.lines(Paths.get(filePath))
+                .findFirst()
+                .get();
+        headerWords.add(header);
 
         return headerWords;
     }
--- src/test/java/com/ryan/kata/parser/InputParserTest.java
+++ src/test/java/com/ryan/kata/parser/InputParserTest.java
@@ -3,15 +3,24 @@ package com.ryan.kata.parser;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 public class InputParserTest {
 
     @Test
-    public void parses_first_line_for_word_to_search() {
+    public void parses_first_line_and_returns_correct_count() throws IOException {
         InputParser inputParser = new InputParser();
-        List<String> wordsToSearch = inputParser.parseHeader("../resources/firstLine.txt");
+        List<String> wordsToSearch = inputParser.parseHeader("src/test/resources/firstLine.txt");
 
         Assert.assertEquals(1, wordsToSearch.size());
+    }
+
+    @Test
+    public void parses_first_line_and_returns_correct_word() throws IOException {
+        InputParser inputParser = new InputParser();
+        List<String> wordsToSearch = inputParser.parseHeader("src/test/resources/firstLine.txt");
+
+        Assert.assertEquals("RYAN", wordsToSearch.get(0));
     }
 }
